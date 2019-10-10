2dd2e4d0376fe27f45ba59f0918842273b8f7372
nerdychap <nerdychap@gmail.com>
2019-10-10 17:43:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var app = angular.module('myApp',[]);
+app.controller('myCtrl', function($scope, $http){
+    //function definition to assign API data to variables
+    display = (info) => {
+        info = info.data;
+        $scope.weather = info.weather[0];
+        $scope.location = info.name + ', ' + info.sys.country;
+        $scope.description = $scope.weather.description;
+        $scope.temp = (info.main.temp).toPrecision(2) + " °C";
+        $scope.image = 'https://openweathermap.org/img/wn/' + $scope.weather.icon + '@2x.png';
+    };
+    
+    //function that is executed at start up to display weather at current location
+    myLocation();
+
+    function myLocation(){
+        //geolocation API used to determine the current location of the user
+        navigator.geolocation.getCurrentPosition((position)=>{
+        var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude.toPrecision(4) + '&lon=' + position.coords.longitude.toPrecision(4)  + '&units=metric&APPID=75500792ea7488c01a878f05c8adf1d9';
+        $http.get(url).then((response)=>{
+            display(response);
+            }); 
+        });
+    };
+    //function executed to get weather at current location
+    $scope.local = () =>{
+        myLocation();
+    };
+    //function used to display data at the location entered into the input text box
+    $scope.getWeather = function(area){
+        $http.get('https://api.openweathermap.org/data/2.5/weather?q=' + area + '&units=metric&APPID=75500792ea7488c01a878f05c8adf1d9').then(function res(response){
+            display(response);
+        });
+    };
+
+});
